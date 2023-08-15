150bc379dfbcb0f31918f02ab59ad67f74b6a728
Maurits <mcb.westbroek@gmail.com>
2023-08-15 10:35:33 +0100

    Add password to URI

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -9,4 +10,4 @@ DEBUG = True
 # Connect to the database
 # Docker database URI
 # run with: docker run -p 5432:5432 -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=fyyur --rm postgres
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres@localhost:5432/fyyur"
+SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost:5432/fyyur"
